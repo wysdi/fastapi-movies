3cc7340487a30d2370422d0bb920474e4fcce593
Wayan Sudiarta <wayan@tix.com.au>
2020-09-04 13:01:00 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from typing import Optional
+from fastapi import FastAPI
+
+import json
+
+app = FastAPI()
+
+#using External Local Data
+with open("movie.json") as f:
+    movies = json.load(f)
+
+
+@app.get('/api/v1/movies')
+async def get_movies():
+    return {"movies": movies}
+
+
+@app.get('/api/v1/movie/{title}')
+async def get_movie(title):
+    movie = [movie for movie in movies if movie['Film'] == title]
+    return {"movie": movie}
+
