1eb7d776d68f77a5b03b38ceabad60b1a94b9536
Wayan Sudiarta <wayan@tix.com.au>
2020-09-10 16:19:21 +0800

    use mysql instead sqllite

--- sql_app/database.py
+++ sql_app/database.py
@@ -2,11 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app/sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app/sql_app.db"
+SQLALCHEMY_DATABASE_URL = 'mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(user='root', password='password', server='127.0.0.1', database='fastapi')
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@127.0.0.1/fastapi"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args={}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
