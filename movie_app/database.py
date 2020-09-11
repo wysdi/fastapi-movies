3f3481357c397d865e82e1b0d84fc75fbe726a1b
Wayan Sudiarta <wayan@tix.com.au>
2020-09-11 15:08:08 +0800

    replace mysqlconnector using pymysql

--- movie_app/database.py
+++ movie_app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(user='root', password='password', server='127.0.0.1', database='movies')
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://{user}:{password}@{server}/{database}'.format(user='root', password='password', server='127.0.0.1', database='movies')
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={}
